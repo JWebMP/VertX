7d4393f767ca99b4fef8764051493ad0abf75e48
GedMarc <ged_marc@hotmail.com>
2026-06-27 01:34:23 +0200

    Jackson migration to version 3

--- src/main/java/com/jwebmp/vertx/implementations/JWebMPVertxBinder.java
+++ src/main/java/com/jwebmp/vertx/implementations/JWebMPVertxBinder.java
@@ -1,15 +1,9 @@
 package com.jwebmp.vertx.implementations;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.json.JsonReadFeature;
-import com.fasterxml.jackson.core.json.JsonWriteFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.AbstractModule;
 import com.guicedee.client.services.lifecycle.IGuiceModule;
 import com.guicedee.client.scopes.CallScope;
 import com.guicedee.client.services.websocket.IGuicedWebSocket;
-import com.guicedee.modules.services.jsonrepresentation.IJsonRepresentation;
-import io.vertx.core.json.jackson.DatabindCodec;
 import net.sf.uadetector.ReadableUserAgent;
 
 
@@ -24,19 +18,8 @@ public class JWebMPVertxBinder extends AbstractModule implements IGuiceModule<JW
         //bind(IGuicedWebSocket.class).to(VertXEventBusBridgeIWebSocket.class);
         bind(IGuicedWebSocket.class).to(VertXStompEventBusBridgeIWebSocket.class);
 
-        ObjectMapper mapper = DatabindCodec.mapper();
-
-        IJsonRepresentation.configureObjectMapper(DatabindCodec.mapper());
-        //ObjectMapper prettyMapper = DatabindCodec.prettyMapper();
-
-        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
-        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-
-        // Enable strict JSON rendering (force field names to have quotes)
-        mapper.configure(JsonWriteFeature.QUOTE_FIELD_NAMES.mappedFeature(), true);
-        mapper.configure(JsonReadFeature.ALLOW_UNQUOTED_FIELD_NAMES.mappedFeature(), true);
-        //mapper.enable(SerializationFeature.QUOTE_FIELD_NAMES);
-        //prettyMapper.enable(SerializationFeature.QUOTE_FIELD_NAMES);
-
+        // Vert.x JSON is explicitly routed through the shared GuicedEE Jackson 3 mapper
+        // via the io.vertx.core.spi.JsonFactory SPI (see com.guicedee.vertx), so the legacy
+        // Jackson 2 DatabindCodec mapper no longer needs configuring here.
     }
 }
